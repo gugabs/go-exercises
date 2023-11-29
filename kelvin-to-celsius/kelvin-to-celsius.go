f4556cf1a8910224d34119c29651614cbbc02f10
Guga <contato.gustavobarbosa@outlook.com>
2023-11-29 18:00:04 -0300

    Kelvin to Celsius

---
+++ kelvin-to-celsius/kelvin-to-celsius.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var waterTemperatureEbullitionInKelvin float32 = 373.2
+
+	fmt.Println(convertKelvinToCelsius(waterTemperatureEbullitionInKelvin))
+}
+
+func convertKelvinToCelsius(k float32) float32 {
+	return k - 273
+}
