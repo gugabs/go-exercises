9980192df4082abb85da954732410ae72ef21caf
Guga <contato.gustavobarbosa@outlook.com>
2023-11-29 21:59:00 -0300

    Set indentation to spaces with tab size 2

--- kelvin-to-celsius/kelvin-to-celsius.go
+++ kelvin-to-celsius/kelvin-to-celsius.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var waterTemperatureEbullitionInKelvin float32 = 373.2
+  var waterTemperatureEbullitionInKelvin float32 = 373.2
 
-	fmt.Println(convertKelvinToCelsius(waterTemperatureEbullitionInKelvin))
+  fmt.Println(convertKelvinToCelsius(waterTemperatureEbullitionInKelvin))
 }
 
 func convertKelvinToCelsius(k float32) float32 {
-	return k - 273
+  return k - 273
 }
