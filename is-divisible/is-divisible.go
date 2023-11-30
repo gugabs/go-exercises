f5487edf39e76bc31f8f65a0fb2c7c26b3ee32ea
Guga <contato.gustavobarbosa@outlook.com>
2023-11-29 21:57:37 -0300

    Is Divisible

---
+++ is-divisible/is-divisible.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  divider := 0
+
+  fmt.Println("Choose the divider:")
+  fmt.Scanf("%d", &divider)
+
+  for i := 1; i <= 100; i++ {
+    if isDivisible(i, divider) {
+      fmt.Printf("O número %d é divisível por %d\n", i, divider)
+    }
+  }
+}
+
+func isDivisible(n int, divider int) bool {
+  return n%divider == 0
+}
